23b751e98b5a2666c2eeeb8109d7924a350a572d
Dragomir Yanev <dragomir.yanev@gmail.com>
2019-06-25 13:05:58 +0300

    Minor color changes

--- src/environment.cpp
+++ src/environment.cpp
@@ -69,7 +69,7 @@ void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer)
 //    renderPointCloud(viewer, filterCloud, "filterCloud");
 
     std::pair<pcl::PointCloud<pcl::PointXYZI>::Ptr, pcl::PointCloud<pcl::PointXYZI>::Ptr> segmentCloud = pointProcessorI->SegmentPlane(filterCloud, POINT_PROCESSOR_MAX_ITERATIONS, POINT_PROCESSOR_DISTANCE_THRESHOLD);
-//    renderPointCloud(viewer, segmentCloud.first, "obstCloud", Color(1, 1, 1));
+//    renderPointCloud(viewer, segmentCloud.first, "obstCloud", Color(1, 0, 0));
     renderPointCloud(viewer, segmentCloud.second, "planeCloud", Color(0, 1, 0));
 
     // Draw roof crop box
@@ -88,8 +88,7 @@ void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer)
     int clusterId = 0;
     std::vector<Color> colors = {Color(1, 1, 0), Color(1, 0, 1),
                                  Color(1, 0, 0), Color(0, 1, 1),
-                                 Color(0.5, 0, 0.5),
-                                 Color(0.5, 0, 0), Color(0, 0.5, 0.5), Color(0, 0.5, 0)};
+                                 Color(1, 1, 1), Color(0, 0, 1),};
 
     for (pcl::PointCloud<pcl::PointXYZI>::Ptr cluster : cloudClusters) {
         if (renderClusterEn) {
@@ -156,7 +155,7 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
         if (renderClusterEn) {
             std::cout << "cluster size ";
             pointProcessor->numPoints(cluster);
-            renderPointCloud(viewer, cluster, "obstCloud" + std::to_string(clusterId), colors[clusterId]);
+            renderPointCloud(viewer, cluster, "obstCloud" + std::to_string(clusterId), colors[clusterId % colors.size()]);
         }
 
         if (renderBoxEn) {
